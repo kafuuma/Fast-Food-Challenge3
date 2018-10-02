b717d05fd74d27bf4b9b3b426855a27c14a732b6
kafuuma <arkafuuma@gmail.com>
2018-10-03 02:27:29 +0300

    feat(fetch all orders): an admin can fetch all orders
    This commit delivers a feature for an admin to fectch all
    orders placed by users
    
    [#160883469]

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -5,6 +5,7 @@ from add_menu import AddMenu
 from get_menu import GetMenu
 from place_order import PlaceOrders
 from update_status import UpdateOrderStatus
+from fetch_orders import GetAllOrders
 from app import app
 import os
 
@@ -19,4 +20,5 @@ api.add_resource(AddMenu, "/api/v1/menu")
 api.add_resource(GetMenu, "/api/v1/menu")
 api.add_resource(PlaceOrders, "/api/v1/users/orders")
 api.add_resource(UpdateOrderStatus, "/api/v1/orders/<int:order_id>")
+api.add_resource(GetAllOrders, "/api/v1/orders")
 
